73cca32c9b66478c218d5ace5c40fafcc6a4f926
Eetu Halinen <eetuhalinen@Eetus-Macbook-Pro.local>
2017-03-09 14:44:37 +0200

    Implemented list using native-base components

--- src/modules/lectures/LecturesView.js
+++ src/modules/lectures/LecturesView.js
@@ -1,12 +1,11 @@
 import React, {PropTypes, Component} from 'react';
 import {
-  Text,
   View,
   ListView,
   StyleSheet
 } from 'react-native';
 
-import {SearchBar, List, ListItem} from 'native-base';
+import {Body, Left, Right, Thumbnail, Content, Container, ListItem, Text} from 'native-base';
 
 class LecturesView extends Component {
   componentDidMount() {
@@ -14,82 +13,36 @@ class LecturesView extends Component {
     this.props.getLectures();
   }
 
-/*
-  getInitialState() {
-    const ds = new ListView.DataSource({
-      rowHasChanged: (r1, r2) => r1 !== r2
-    });
-    return {
-      dataSource: ds
-    };
-  },
-
-  componentWillReceiveProps(nextProps) {
-    this.setState({
-      dataSource: this.state.dataSource.cloneWithRows(nextProps.experts.data)
-    });
-  },
-*/
-  renderRow(rowData, sectionID) {
-    return (
-      <ListItem
-        roundAvatar
-        key={sectionID}
-        title={rowData.name}
-        subtitle={
-          <View style={styles.subtitleView}>
-            <Text>{rowData.title}</Text>
-            <Text>{rowData.area}</Text>
-            <View style={styles.tagsWrapper}>
-              {rowData.subjects.map((tag, index) => (
-                <Text key={index} style={styles.subtitleText}>{tag}</Text>
-              ))}
-            </View>
-          </View>
-        }
-        avatar={{uri: rowData.photograph}}
-        />
-    );
-  }
-
-  changeFilter(e) {
-    console.log(e);
-  }
-
   render() {
     return (
-      <View style={styles.container}>
-        <Text>{JSON.stringify(this.props.lectures)}</Text>
-      </View>
+      <Container>
+        <Content>
+          {
+            this.props.lectures.map((lecture) => (
+              <ListItem avatar key={lecture.id}>
+                <Left>
+                  <Thumbnail source={require('../../../images/pepperoni.png')} />
+                </Left>
+                <Body>
+                  <Text>{lecture.title}</Text>
+                  <Text note>{lecture.dates}</Text>
+                </Body>
+                <Right>
+                  <Text>></Text>
+                </Right>
+              </ListItem>
+            ))
+          }
+        </Content>
+      </Container>
+
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1
-  },
-  subtitleView: {
-    paddingLeft: 10,
-    paddingTop: 5
-  },
-  subtitleText: {
-    marginRight: 7,
-    color: 'white',
-    backgroundColor: '#D8D8D8',
-    borderRadius: 5,
-    paddingHorizontal: 15,
-    paddingVertical: 2,
-    marginTop: 7
-  },
-  tagsWrapper: {
-    alignItems: 'flex-start',
-    flexDirection: 'row',
-    flexWrap: 'wrap'
-  },
-  list: {
-    flex: 1,
-    marginTop: 0
+  containerStyle: {
+    backgroundColor: 'red'
   }
 });
 
