84dcf68aea36b32f789191c71826c0aaa606fd6f
Janne <janne.jappinen@protonmail.com>
2017-03-14 12:03:17 +0200

    Changed text layout in LectureDetails and added id to fetch info with

--- src/modules/lectureDetails/LectureDetailsView.js
+++ src/modules/lectureDetails/LectureDetailsView.js
@@ -6,21 +6,21 @@ import { Container, Footer, FooterTab, Content, Button, Text, H1, H2, H3 } from 
 class LectureDetailsView extends Component {
   componentDidMount() {
     console.log('Hello');
+    this.props.getLectureDetails(34658);
   }
 
   render() {
     return (
       <Container>
           <Content>
-              <Text>Theme of the lecture:</Text>
-              <H3>What is entrepreneurship?</H3>
-              <Text>Date of the lecture:</Text>
-              <H3>12.5.2017</H3>
-              <Text>Location:</Text>
-              <H3>Helsinki Internation School</H3>
-              <Text>Short description of the lecture:</Text>
-              <H3>The lecture is part of the course, "Introduction to entrepreneurship,"
-                is designed for high school students with little or no background in the field.</H3>
+              <Text note>Theme of the lecture:</Text>
+              <Text>{this.props.lectureDetails.title}</Text>
+              <Text note>Date of the lecture:</Text>
+              <Text>{new Date(this.props.lectureDetails.dates).toLocaleDateString('fi-FI')}</Text>
+              <Text note>Location:</Text>
+              <Text>{this.props.lectureDetails.area}</Text>
+              <Text note>Short description of the lecture:</Text>
+              <Text>{this.props.lectureDetails.description}</Text>
           </Content>
           <Footer>
             <FooterTab>
