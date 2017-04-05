e403772b0ca57322c4a926d6ad71c662292676c4
Ossi Tammi <ossi.tammi@futurice.com>
2017-04-05 13:26:40 +0300

    Splash screen for iOS

--- index.ios.js
+++ index.ios.js
@@ -1,11 +1,20 @@
-import {Provider} from 'react-redux';
+import { Provider } from 'react-redux';
+import React, { Component } from 'react';
+import { AppRegistry } from 'react-native';
+import SplashScreen from 'react-native-smart-splash-screen';
 import store from './src/redux/store';
 import AppViewContainer from './src/modules/AppViewContainer';
 
-import React, {Component} from 'react';
-import {AppRegistry} from 'react-native';
-
 class XPRT extends Component {
+  componentDidMount() {
+    console.log(SplashScreen);
+    SplashScreen.close({
+      animationType: SplashScreen.animationType.scale,
+      duration: 800,
+      delay: 300,
+    });
+  }
+
   render() {
     return (
       <Provider store={store}>
