32907cea2a1671c3f8a143fd691b5d132e51ddd8
kingHUGSY <eetuksi@gmail.com>
2017-04-11 12:30:53 +0300

    LectureDetailsView looks more like in designs

---
+++ src/modules/lectureDetails/LectureDetailStyles.js
@@ -0,0 +1,70 @@
+const styles = {
+  profileGrid: {
+    paddingBottom: 10,
+  },
+  profileGridCol: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: 200,
+  },
+  labelStyle: {
+    color: '#15a369',
+    fontSize: 13,
+    paddingBottom: 5,
+    paddingTop: 15,
+  },
+  headlineStyle: {
+    color: '#cccccc',
+    fontSize: 15,
+    paddingTop: 10,
+  },
+  rowBorder: {
+    borderBottomWidth: 1,
+    borderColor: '#cccccc',
+    paddingBottom: 20,
+  },
+  logoutButton: {
+    color: '#f0ad4e',
+  },
+  feedbackButton: {
+    padding: 5,
+  },
+  iconMail: {
+    width: 20,
+    height: 15,
+  },
+  iconPhone: {
+    width: 20,
+    height: 20,
+  },
+  iconFeedback: {
+    width: 20,
+    height: 18,
+  },
+  iconEdit: {
+    width: 22,
+    height: 22,
+    margin: 10,
+  },
+  contactText: {
+    marginTop: 5,
+    height: 20,
+  },
+  flexContainer: {
+    flex: 1,
+    justifyContent: 'space-between',
+  },
+  editPenAlignRight: {
+    alignItems: 'flex-end',
+  },
+  avatarMedium: {
+    width: 80,
+    height: 80,
+    borderRadius: 40,
+  },
+  boldText: {
+    fontWeight: '700',
+  },
+};
+
+module.exports = styles;
