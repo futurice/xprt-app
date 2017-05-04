c314b7d556e6911309fac29a5bb3359233448c1b
MT <monkeymake@hotmail.com>
2017-05-04 14:35:10 +0300

    miniscule fixes to conflicts i loginnag

--- src/components/LoginNag.js
+++ src/components/LoginNag.js
@@ -14,18 +14,18 @@ export default class LoginNag extends Component {
     return (
       <Container>
         <Content padder>
-          { __DEV__ && devLogin && <Button style={styles.devLogin} full onPress={devLogin}>
-            <Text style={styles.devLoginText}>DEVELOPER LOGIN</Text>
-          </Button> }
-          <Button style={styles.loginButton} full onPress={() => openLogin('Login')}>
-            <Text style={styles.loginButtonText}>
-              LOG IN WITH HUNDR
-           </Text>
-            <Text style={styles.loginButtonTextBold}>
-              ED
-           </Text>
-          </Button>
         </Content>
+        { __DEV__ && devLogin && <Button style={styles.devLogin} full onPress={devLogin}>
+          <Text style={styles.devLoginText}>DEVELOPER LOGIN</Text>
+        </Button> }
+        <Button style={styles.loginButton} full onPress={() => openLogin('Login')}>
+          <Text style={styles.loginButtonText}>
+            LOG IN WITH HUNDR
+         </Text>
+          <Text style={styles.loginButtonTextBold}>
+            ED
+         </Text>
+        </Button>
       </Container>
     );
   }
