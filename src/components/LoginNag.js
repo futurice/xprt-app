d183051e1f8494f53be0e6035096268dd55baebb
MT <monkeymake@hotmail.com>
2017-05-04 14:26:27 +0300

    fixed a conflict with new login buttons

--- src/components/LoginNag.js
+++ src/components/LoginNag.js
@@ -4,25 +4,27 @@ import styles from './ProfileStyles';
 
 export default class LoginNag extends Component {
   static propTypes = {
-    text: PropTypes.string.isRequired,
     openLogin: PropTypes.func.isRequired,
     devLogin: PropTypes.func.isRequired,
   };
 
   render() {
-    const { text, openLogin, devLogin } = this.props;
+    const { openLogin, devLogin } = this.props;
 
     return (
       <Container>
         <Content padder>
-          <Text>{ text }</Text>
-          <Button dark full onPress={() => openLogin('Login')}>
-            <Text style={styles.logoutButton}>LOG IN WITH HUNDRED</Text>
-          </Button>
-
-          { __DEV__ && devLogin && <Button dark full onPress={devLogin}>
-            <Text style={styles.logoutButton}>DEVELOPER LOGIN</Text>
+          { __DEV__ && devLogin && <Button style={styles.devLogin} full onPress={devLogin}>
+            <Text style={styles.devLoginText}>DEVELOPER LOGIN</Text>
           </Button> }
+          <Button style={styles.loginButton} full onPress={() => openLogin('Login')}>
+            <Text style={styles.loginButtonText}>
+              LOG IN WITH HUNDR
+           </Text>
+            <Text style={styles.loginButtonTextBold}>
+              ED
+           </Text>
+          </Button>
         </Content>
       </Container>
     );
