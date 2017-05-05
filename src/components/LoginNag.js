f3af71dc0957757ed2215972e6e6c8fde6997660
Rasmus Eskola <fruitiex@gmail.com>
2017-05-05 12:35:25 +0300

    Wrap page in <Content> and add text back

--- src/components/LoginNag.js
+++ src/components/LoginNag.js
@@ -1,5 +1,5 @@
 import React, { Component, PropTypes } from 'react';
-import { Button, Container, Text } from 'native-base';
+import { Button, Content, Container, Text } from 'native-base';
 import styles from './ProfileStyles';
 
 export default class LoginNag extends Component {
@@ -9,21 +9,24 @@ export default class LoginNag extends Component {
   };
 
   render() {
-    const { openLogin, devLogin } = this.props;
+    const { openLogin, devLogin, text } = this.props;
 
     return (
       <Container>
-        { __DEV__ && devLogin && <Button style={styles.devLogin} full onPress={devLogin}>
-          <Text style={styles.devLoginText}>DEVELOPER LOGIN</Text>
-        </Button> }
-        <Button style={styles.loginButton} full onPress={() => openLogin('Login')}>
-          <Text style={styles.loginButtonText}>
-            LOG IN WITH HUNDR
-          </Text>
-          <Text style={styles.loginButtonTextBold}>
-            ED
-          </Text>
-        </Button>
+        <Content padder>
+          <Text style={styles.loginText}>{ text }</Text>
+          { __DEV__ && devLogin && <Button style={styles.devLogin} full onPress={devLogin}>
+            <Text style={styles.devLoginText}>DEVELOPER LOGIN</Text>
+          </Button> }
+          <Button style={styles.loginButton} full onPress={() => openLogin('Login')}>
+            <Text style={styles.loginButtonText}>
+              LOG IN WITH HUNDR
+            </Text>
+            <Text style={styles.loginButtonTextBold}>
+              ED
+            </Text>
+          </Button>
+        </Content>
       </Container>
     );
   }
